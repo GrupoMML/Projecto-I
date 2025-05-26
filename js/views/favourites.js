65aa24ebcaa91b8204dd0eec03a88a35c2cf0d64
Miguyy <miguelangelo123machado@gmail.com>
2025-05-26 19:43:13 +0100

    ficheiros view criados

--- js/views/favourites.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.heart-icon').forEach(icon => {
-    icon.addEventListener('click', () => {
-    icon.classList.toggle('fa-solid')
-    icon.classList.toggle('fa-regular')
-    icon.classList.toggle('clicked')
-    })
-})
