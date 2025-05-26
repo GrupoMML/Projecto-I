bbc9f565a4700981bc9646cff666c6fa34a83f78
Miguyy <miguelangelo123machado@gmail.com>
2025-05-26 23:04:16 +0100

    favourites model - done

--- js/models/favouritesModel.js
+++ js/models/favouritesModel.js
@@ -1,11 +1,10 @@
 //------------------ FAVOURITES CLASS ----------------------
 //----------------------------------------------------------
 class Favourites {
-    constructor(disciplineName, disciplineTeacher) {
-        this.disciplineName = disciplineName;
-        this.disciplineTeacher = {
-            name: disciplineTeacher.name,
-            image: disciplineTeacher.image
+    constructor(favTeacher) {
+        this.favTeacher = {
+            name: favTeacher.name,
+            image: favTeacher.image
         };
     }
 }
